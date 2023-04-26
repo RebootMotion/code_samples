5f36ce0845748cdd1a77ba8fc0dbb90d647c44ea
Robert Calise <rcalise@rebootmotion.com>
2023-04-26 10:55:14 -0700

    Add code samples for Requested Analysis

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# code_samples
-Repository of code samples for working with the Reboot Motion API
+# Reboot Motion Code Samples
+
+This repository contains a library of code samples for working with the Reboot Motion API and is maintained by the Reboot Motion engineering team. Access to the Reboot Motion API is only open to active Reboot Motion customers and requires an API Key. Documentation for working with the API can be found at: [https://api.rebootmotion.com/docs](https://api.rebootmotion.com/docs)
+
+- [Python Code Samples](./python)
---
+++ python/request_analysis.py
@@ -0,0 +1,61 @@
+import os
+
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_BASE = "https://api.rebootmotion.com"
+API_HEADER = {'x-api-key': os.getenv("API_KEY")}
+CSV_FILE = "../resources/csv/movements_with_pitch_types.csv"
+DOM_HAND = "RHA"        # Right Handed
+MOVEMENT_TYPE_ID = 2    # baseball-pitching
+MOCAP_TYPE_ID = 104     # Hawk-Eye High Frame Rate
+
+
+def main():
+    csv_df = pd.read_csv(CSV_FILE)
+
+    primary_df = csv_df[csv_df["PitchType"] == "Fastball"]
+    primary_segment_id = requests.post(
+        f"{API_BASE}/player_group_segments",
+        headers=API_HEADER,
+        json={
+            "external_context_ids": (
+                primary_df["Identifier"].tolist()
+            ),
+            "movement_type_id": MOVEMENT_TYPE_ID,
+            "mocap_type_id": MOCAP_TYPE_ID,
+            "dom_hand": DOM_HAND
+        }
+    ).json()["analysis_segment_id"]
+
+    comparison_df = csv_df[csv_df["PitchType"] == "Curveball"]
+    comparison_segment_id = requests.post(
+        f"{API_BASE}/player_group_segments",
+        headers=API_HEADER,
+        json={
+            "external_context_ids": comparison_df["Identifier"].tolist(),
+            "movement_type_id": MOVEMENT_TYPE_ID,
+            "mocap_type_id": MOCAP_TYPE_ID,
+            "dom_hand": DOM_HAND
+        }
+    ).json()["analysis_segment_id"]
+
+    requested_analysis = requests.post(
+        f"{API_BASE}/requested_analyses",
+        headers=API_HEADER,
+        json={
+            "name": "Example Pitcher Fastballs vs Curveballs",
+            "primary_analysis_segment_id": int(primary_segment_id),
+            "comparison_analysis_segment_id": int(comparison_segment_id),
+            "status": "requested"
+        }
+    )
+
+    print(requested_analysis.json())
+
+
+if __name__ == "__main__":
+    main()
